cbcd08031a2ec026def707816d3e504883e92286
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2016-04-21 09:08:15 +0100

    Removed base physical attack stat, don't want to inheret anymore, just use a base stat.  Base stat no longer abstract, added comments, added battle value, removed set from name, description and stat type, replaced setting them with a constructor

--- Assets/RPG/Scripts/Prototype/Stats/BaseStat.cs
+++ Assets/RPG/Scripts/Prototype/Stats/BaseStat.cs
@@ -1,11 +1,15 @@
 ﻿[System.Serializable]
-public abstract class BaseStat
+public class BaseStat
 {
     private string _name;
     private string _description;
-    private int _baseValue;
-    private int _modifiedValue;
     private StatTypes _type;
+    private int _baseValue;     //comes from level/animation curve, is only set on level up
+    //modified is a culmination of weapon, armour, accessory and any other modifiers. when getting, return base plus modified, allows for modified to potentially lower base
+    //character has public getters for these stats, allows for effects such as lowering a stat to base value or restoring a stat to normal aka default modified value
+    private int _modifiedValue;
+    private int _battleValue;   //set to modified on battle begin, this stat is then modified during battle using effects like haste, meltdown etc.
+                                //battle hp cannot go above modified value
 
     public enum StatTypes
     {
@@ -16,16 +20,21 @@ public abstract class BaseStat
         MAGDEF
     }
 
+    public BaseStat(string name, string description, StatTypes type)
+    {
+        _name = name;
+        _description = description;
+        _type = type;
+    }
+
     public string StatName
     {
         get { return _name; }
-        set { _name = value; }
     }
 
     public string StatDescription
     {
         get { return _description; }
-        set { _description = value; }
     }
 
     public int StatBaseValue
@@ -43,6 +52,11 @@ public abstract class BaseStat
     public StatTypes StatType
     {
         get { return _type; }
-        set { _type = value; }
+    }
+
+    public int BattleValue
+    {
+        get { return _battleValue; }
+        set { _battleValue = value; }
     }
 }
